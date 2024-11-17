9337ff64c859ef9373d1f7e03e063a4091cf2612
ArthurX10 <153871082+ArthurX10@users.noreply.github.com>
2024-11-17 10:34:51 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void imprimir_matriz(int **mat, int linha, int coluna) {
+    for (int i = 0; i < linha; i++) {
+        for (int j = 0; j < coluna; j++) {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main() {
-    int i, j;
-    int linha, coluna;
+    int i, j, linha, coluna;
     int **mat;
 
     scanf("%d %d", &linha, &coluna);
 
-    if (linha == 0 || coluna == 0) {
-        printf("matriz vazia\n");
-        return 0;
-    }
+    mat = (int**)malloc(linha * sizeof(int*));
+    if (mat == NULL) return 1;
 
-    mat = (int **)malloc(linha * sizeof(int *));
     for (i = 0; i < linha; i++) {
-        mat[i] = (int *)malloc(coluna * sizeof(int));
+        mat[i] = (int*)malloc(coluna * sizeof(int));
+        if (mat[i] == NULL) return 1;
     }
 
     for (i = 0; i < linha; i++) {
@@ -24,15 +30,7 @@ int main() {
         }
     }
 
-    for (i = 0; i < linha; i++) {
-        for (j = 0; j < coluna; j++) {
-            printf("%d", mat[i][j]);
-            if (j < coluna - 1) {
-                printf(" ");
-            }
-        }
-        printf("\n");
-    }
+    imprimir_matriz(mat, linha, coluna);
 
     for (i = 0; i < linha; i++) {
         free(mat[i]);
