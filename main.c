ca93412919c6a87ad1f5986136c7c2b5c2b45265
ArthurX10 <153871082+ArthurX10@users.noreply.github.com>
2024-11-17 15:07:59 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,49 +1,48 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-
-int main(){
-    int **mat, linha, coluna, i, j;
+int main() {
+    int linha, coluna, **mat, i, j;
 
 
     scanf("%d %d", &linha, &coluna);
 
 
-    if(linha == 0 || coluna==0){
-        printf("[matriz vazia]");return 0;
+    if (linha == 0 || coluna == 0) {
+        printf("[matriz vazia]\n");
+        return 0;
     }
 
-    mat = (int**)malloc(linha*sizeof(int*));
-        for(int i = 0; i< linha; i++){
-            mat[i] = (int*)malloc(coluna *sizeof(int));
-        }
+
+    mat = (int**)malloc(linha * sizeof(int*));
+
+    for (i = 0; i < linha; i++) {
+        mat[i] = (int*)malloc(coluna * sizeof(int));
+    }
 
 
-       for(i = 0; i < linha; i++){
-           for(j = 0; j< coluna; j++){
-                scanf("%d", &mat[i][j]);
+    for (i = 0; i < linha; i++) {
+        for (j = 0; j < coluna; j++) {
+            scanf("%d", &mat[i][j]);
         }
-        }
-         printf("'");
-        for(i = 0; i < linha; i++){
-           for(j = 0; j < coluna; j++){
-                printf("%d", mat[i][j]);
-                if (j < coluna - 1) {
+    }
+
+
+    for (i = 0; i < linha; i++) {
+        for (j = 0; j < coluna; j++) {
+            printf("%d", mat[i][j]);
+            if (j < coluna - 1) {
                 printf(" ");
             }
         }
-        if (i < linha - 1) {
-            printf("\n");
-        } else {
-        printf("'");}
+        printf("\n");
     }
 
 
-
-
-        for (i = 0; i < linha; i++) {
+    for (i = 0; i < linha; i++) {
         free(mat[i]);
     }
     free(mat);
-return 0;
+
+    return 0;
 }
