ea9a971742d5a4b9320dd2d1a223e679979d3eed
ArthurX10 <153871082+ArthurX10@users.noreply.github.com>
2024-11-17 08:09:28 -0300

    Atualizar o main.c

--- main.c
+++ main.c
@@ -1,45 +1,48 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    int i, j;
+    int linha, coluna;
+    int **mat;
 
-int main(){
-  int i, j;
-  int linha, coluna;
-  int **mat;
+ 
+    scanf("%d", &linha);
+    scanf("%d", &coluna);
 
-  scanf("%d", &linha);
-  scanf("%d", &coluna);
-
-  if(linha == 0 || coluna == 0){
-    printf("matriz vazia\n");
-    return 0;
-  }
+    
+    if (linha == 0 || coluna == 0) {
+        printf("matriz vazia\n");
+        return 0;
+    }
 
   
-  mat = (int**)malloc(linha * sizeof(int*));
-
-  for(i=0; i<linha;i++){
-   mat[i] =(int*)malloc(coluna*sizeof(int));
-
-  }
-
-  for(i=0; i<linha; i++){
-    for(j=0; j<coluna; j++){
-      scanf("%d", &mat[i][j]);
+    mat = (int **)malloc(linha * sizeof(int *));
+    for (i = 0; i < linha; i++) {
+        mat[i] = (int *)malloc(coluna * sizeof(int));
     }
-  }
 
-  for(i=0; i<linha; i++){
-    for(j=0; j<coluna; j++){
-      printf("%d", mat[i][j]); 
+   
+    for (i = 0; i < linha; i++) {
+        for (j = 0; j < coluna; j++) {
+            scanf("%d", &mat[i][j]);
+        }
     }
-    printf("\n");
-  }
 
-  for (i = 0; i < linha; i++) {
-      free(mat[i]);
-  }
-free(mat);
-  return 0;
+  
+    for (i = 0; i < linha; i++) {
+        for (j = 0; j < coluna; j++) {
+            printf("%d ", mat[i][j])
+        }
+        printf("\n");
+    }
+
+   
+    for (i = 0; i < linha; i++) {
+        free(mat[i]);
+    }
+    free(mat);
+
+    return 0;
 }
 
