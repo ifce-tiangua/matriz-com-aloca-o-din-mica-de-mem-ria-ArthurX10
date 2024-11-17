e29a5d3467f1060e9e11fcabdd073b4664acdaab
ArthurX10 <153871082+ArthurX10@users.noreply.github.com>
2024-11-17 13:46:10 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,59 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void imprimir_matriz(int **mat, int linha, int coluna) {
-    if (linha == 0 || coluna == 0) {
-        printf("matriz vazia");
-        return;
-    }
+#include<stdio.h>
+#include<stdlib.h>
 
 
-    for (int i = 0; i < linha; i++) {
-        for (int j = 0; j < coluna; j++) {
-            printf("%d", mat[i][j]);
-            if (j < coluna - 1) {
-                printf(" ");
-            }
-        }
-        if (i < linha - 1) {
-            printf(" "); 
-        }
-        printf("\n"); 
-    }
-   
-}
+int main(){
+    int **mat, linha, coluna, i, j;
 
-int main() {
-    int i, j, linha, coluna;
-    int **mat;
 
     scanf("%d %d", &linha, &coluna);
 
-    if (linha == 0 || coluna == 0) {
-        printf("[matriz vazia]");
-        return 0;
+
+    if(linha == 0 || coluna==0){
+        printf("[matriz vazia]");return 0;
     }
 
-    mat = (int**)malloc(linha * sizeof(int*));
-    if (mat == NULL) return 1;
-
-    for (i = 0; i < linha; i++) {
-        mat[i] = (int*)malloc(coluna * sizeof(int));
-        if (mat[i] == NULL) return 1;
-    }
-
-    for (i = 0; i < linha; i++) {
-        for (j = 0; j < coluna; j++) {
-            scanf("%d", &mat[i][j]);
+    mat = (int**)malloc(linha*sizeof(int*));
+        for(int i = 0; i< linha; i++){
+            mat[i] = (int*)malloc(coluna *sizeof(int));
         }
-    }
 
-    imprimir_matriz(mat, linha, coluna);
 
-    for (i = 0; i < linha; i++) {
+       for(i = 0; i < linha; i++){
+           for(j = 0; j< coluna; j++){
+                scanf("%d", &mat[i][j]);
+        }
+        }
+
+        for(i = 0; i < linha; i++){
+           for(j = 0; j < coluna; j++){
+                printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+        }
+
+        for (i = 0; i < linha; i++) {
         free(mat[i]);
     }
     free(mat);
-
-    return 0;
+return 0;
 }
