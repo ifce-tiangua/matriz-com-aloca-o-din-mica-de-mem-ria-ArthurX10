33292265152d95274ca4ab9060d64483a60438c3
ArthurX10 <153871082+ArthurX10@users.noreply.github.com>
2024-11-18 09:57:32 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,36 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LINHA 20
-#define COLUNA 20
-
 int main() {
-    int linha = LINHA, coluna = COLUNA, **mat, i, j;
-
-
+    int linha, coluna, **mat, i, j;
+    
     scanf("%d %d", &linha, &coluna);
 
-
+  
     if (linha == 0 || coluna == 0) {
         printf("[matriz vazia]\n");
         return 0;
     }
 
-
-    mat = (int**)malloc(linha * sizeof(int*));
-
+    
+    mat = (int**)calloc(linha, sizeof(int*));  
     for (i = 0; i < linha; i++) {
-        mat[i] = (int*)malloc(coluna * sizeof(int));
+        mat[i] = (int*)calloc(coluna, sizeof(int)); 
     }
 
-
+    
     for (i = 0; i < linha; i++) {
         for (j = 0; j < coluna; j++) {
             scanf("%d", &mat[i][j]);
         }
     }
 
-
+  
     for (i = 0; i < linha; i++) {
         for (j = 0; j < coluna; j++) {
             printf("%d", mat[i][j]);
@@ -43,7 +38,7 @@ int main() {
         }
     }
 
-
+  
     for (i = 0; i < linha; i++) {
         free(mat[i]);
     }
